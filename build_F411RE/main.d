6441e7d2140583fd6b4918dae875dc56753c477d
Ruslanisheya <Ruslanisheya@gmail.com>
2025-11-13 13:06:25 +0700

    goida

---
+++ build_F411RE/main.d
@@ -0,0 +1,5 @@
+build_F411RE/main.o: Core/Src/main.c Core/Src/../Inc/init.h \
+ Core/Src/../Src/init.c Core/Src/../Src/../Inc/init.h
+Core/Src/../Inc/init.h:
+Core/Src/../Src/init.c:
+Core/Src/../Src/../Inc/init.h:
